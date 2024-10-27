56da48ee0f2faaaf4767c5934cb53bef6b9b61bf
Ty TN <tytn9197@gmail.com>
2024-10-27 11:59:52 +0700

    init. react native 0.76

---
+++ App.tsx
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View>
+        <Text style={{fontSize: 50}}>
+          {' '}
+          ------------------------------------------------TYTN
+          ------------------------------------------------
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
