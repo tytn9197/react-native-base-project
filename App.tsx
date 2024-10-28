2f2f4dc4f7aff30c30fcc90a2d71e32c8d2403db
Ty TN <tytn9197@gmail.com>
2024-10-28 23:33:41 +0700

    add. libs and demo of react navigation

--- App.tsx
+++ App.tsx
@@ -6,19 +6,27 @@
  */
 
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import {SafeAreaView, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+function HomeScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </SafeAreaView>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <View>
-        <Text style={{fontSize: 50}}>
-          {' '}
-          ------------------------------------------------TYTN
-          ------------------------------------------------
-        </Text>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
