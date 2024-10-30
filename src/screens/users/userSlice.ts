d0a6d2bfea34e564d26863ed9cf840f63f08a8c6
Ty TN <tytn9197@gmail.com>
2024-10-30 17:18:33 +0700

    user slice

---
+++ src/screens/users/userSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { RootState } from '../../redux/store'
+
+
+interface User {
+  id: string
+  name: string
+}
+
+const initialState: User[] = [
+  { id: '0', name: 'Tianna Jenkins' },
+  { id: '1', name: 'Kevin Grant' },
+  { id: '2', name: 'Madison Price' }
+]
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {}
+})
+
+export default usersSlice
+
+export const selectAllUsers = (state: RootState) => state.users
+
+export const selectUserById = (state: RootState, userId: string | null) =>
+  state.users.find(user => user.id === userId)
