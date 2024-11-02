0b6ae41e054c165f29a0a3a42ebd1a2b061e7e20
Ty TN <tytn9197@gmail.com>
2024-11-02 22:00:40 +0700

    change. nav constants

--- src/constants/APP_CONSTANTS.ts
+++ src/constants/APP_CONSTANTS.ts
@@ -1,6 +1,3 @@
 export const APP_CONST = {
-    NAV: {
-        DETAILS: "Details",
-        HOME: "HOME"
-    }
+
 }
---
+++ src/constants/NAV.ts
@@ -0,0 +1,19 @@
+export const NAV = {
+  // root navigator
+  // authentication navigator
+  SIGN_IN: 'SIGN_IN',
+  SIGN_UP: 'SIGN_UP',
+
+  // authenticated navigator
+  DETAILS: 'DETAILS',
+  HOME: 'HOME',
+  MY_DRAWER: {
+    SELF: 'MY_DRAWER',
+    POST_LIST: 'POST_LIST_DRAWER',
+    USER: 'USER',
+  },
+
+  // common screens
+  ABOUT: "ABOUT",
+  HELP: "HELP"
+};
