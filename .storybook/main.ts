054c316f796a405a52e385841334bdc176bac9f1
Ty TN <tytn9197@gmail.com>
2024-11-18 22:38:33 +0700

    addon of storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,12 @@ import { StorybookConfig } from '@storybook/react-native';
 
 const main: StorybookConfig = {
   stories: ['../src/components/**/**/*.stories.?(ts|tsx|js|jsx)'],
-  addons: ['@storybook/addon-ondevice-controls', '@storybook/addon-ondevice-actions'],
+  addons: [
+    '@storybook/addon-ondevice-notes',
+    '@storybook/addon-ondevice-controls',
+    '@storybook/addon-ondevice-backgrounds',
+    '@storybook/addon-ondevice-actions',
+  ],
 };
 
 export default main;
