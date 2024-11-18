fdaba576e18965af695f93145114c5c3797d60a1
Ty TN <tytn9197@gmail.com>
2024-11-18 23:36:19 +0700

    fix, log box of reanimated

--- index.js
+++ index.js
@@ -5,9 +5,19 @@ import './gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {
+  configureReanimatedLogger,
+  ReanimatedLogLevel,
+} from 'react-native-reanimated';
+
+// This is the default configuration
+configureReanimatedLogger({
+  level: ReanimatedLogLevel.warn,
+  strict: true, // Reanimated runs in strict mode by default
+});
 
 let AppEntryPoint = App;
 
-AppEntryPoint = require("./.storybook").default;
+AppEntryPoint = require('./.storybook').default;
 
 AppRegistry.registerComponent(appName, () => AppEntryPoint);
