365c02e658f5d6b884691ab4c5d6c49836366158
Ty TN <tytn9197@gmail.com>
2024-11-21 15:13:10 +0700

    delete. example of storybook

--- src/components/atoms/Task/styles.ts
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from "react-native"
-
-const TaskStyles = StyleSheet.create({
-        listItem: {
-            flexDirection: "row",
-        }
-    })
-
-export default TaskStyles
--- src/components/molecules/TaskList/styles.ts
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from "react-native"
-
-const TaskListStyles = StyleSheet.create({
-        listItem: {
-            flexDirection: "row",
-        }
-    })
-
-export default TaskListStyles
